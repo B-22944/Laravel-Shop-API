bd486ab6f70e85cc065463afc988910e951b04d2
Usama Hassan <ahmarkhan1233456@gmail.com>
2022-09-13 17:45:28 +0500

    API named routes modiefied

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //Listing the products category
-Route::get('listCategory',[CategoryController::class,'listCategory'])->name('show_categories');
+Route::get('listCategory',[CategoryController::class,'listCategory'])->name('categories.show_categories');
 //Listing the products
-Route::get('listProduct/{id?}',[ProductController::class,'listProduct'])->name('show_products');;
+Route::get('listProduct/{id?}',[ProductController::class,'listProduct'])->name('products.show_products');;
 //Searching the data on the basis of category_id
-Route::get('category/{id}',[ProductController::class,'category'])->name('search_category');;
+Route::get('category/{id}',[ProductController::class,'category'])->name('products.search_category');;
