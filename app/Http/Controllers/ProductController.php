baf0fbc4f41f71f29501d418f44730ce69f75b4a
Usama Hassan <ahmarkhan1233456@gmail.com>
2022-09-16 11:44:09 +0500

    Product API with jason response

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,20 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Product;
-use App\Http\Resources\ProductResource;
+use App\Http\Requests\ApiRequest;
 
 class ProductController extends Controller
 {
-    //Searching for Categories
+    /*
+    Searching for Categories
+    */
     public function category($id){
 
         try {
-            //Elequent Query for searching category id from products table
+            /*
+            Elequent Query for searching category id from products table
+            */
             $categories = Product::where('category_id', '=', $id)->get();
-            return $categories;    
+            return response()->json(['categories'=>$categories],404);    
         } 
         catch (Exception $e) {
-            return "NO Data Found";
+            return response()->json(['message'=>'Error Error: Not Found'],404);
         }
         
     }
@@ -27,23 +31,19 @@ class ProductController extends Controller
     API CRUD Operations
     */
 
-    //Fetching all the product from database
+    /*
+    Fetching all the product from database
+    */
     public function showAll(){
         $products = Product::all();
-        return ProductResource::collection($products);
+        return response()->json(['products'=>$products],200);
     }
 
-    //Storing Data through Postman in Raw jason format 
-    public function store(Request $request){
+    /*
+    Storing Data through Postman in Raw jason format 
+    */
+    public function store(ApiRequest $request){
         
-        $request->validate([
-            'image'=>'required',
-            'title'=>'required',
-            'price'=>'required',           
-            'description'=>'required',
-            'category_id'=>'required',
-        ]);
-
         try {
             $product = new Product;
             $product->image = $request->image;        
@@ -51,53 +51,62 @@ class ProductController extends Controller
             $product->price = $request->price;
             $product->description = $request->description;
             $product->category_id = $request->category_id;
-                if ($product->save()) {
-                    return new ProductResource($product);
-                }
+            $product->save();
+                return response()->json(['message'=>'Added Success'],201);
+                
         } catch (\Exception $e) {
-            // Set a response code
-            var_dump(http_response_code(404));
+            /* 
+            Set a response code for error: not found
+            */
+            return response()->json(['message'=>'Error: Not Found'],404);
         }
     }
 
-    //Fetching the product from database with id
+    /*
+    Fetching the product from database with id
+    */
     public function show($id){
-        $product = Product::findOrFail($id);
-        return new ProductResource($product);
+        try {
+            $product = Product::findOrFail($id);
+            return response()->json(['product'=>$product],200);
+        } catch (\Exception $e) {
+            return response()->json(['message'=>'Error: Not Found'],404);
+        }
+        
     }
 
-    //Storing Data through Postman in Raw jason format
-    public function update(Request $request, $id){
-
-        $request->validate([
-            'image'=>'required',
-            'title'=>'required',
-            'price'=>'required',           
-            'description'=>'required',
-            'category_id'=>'required',
-        ]);
-        
+    /*
+    Storing Data through Postman in Raw jason format
+    */
+    public function update(ApiRequest $request, $id){
+  
         try {
-            
+
             $product = Product::findOrFail($id);
             $product->image = $request->image;        
             $product->title = $request->title;
             $product->price = $request->price;
             $product->description = $request->description;
             $product->category_id = $request->category_id;
-                if ($product->save()) {
-                    return new ProductResource($product);
-            }
+            $product->save();
+                return response()->json(['message'=>'Updated Success'],201);
+            
         } catch (\Exception $e) {
-            // Set a response code
-            var_dump(http_response_code(404));
+            /* 
+            Set a response code for error: not found
+            */
+            return response()->json(['message'=>'Error Error: Not Found'],404);
         }
     }
 
     public function destroy($id){
-        $product = Product::findOrFail($id);
-        if ($product->delete()) {
-            return new ProductResource($product);
+        try {
+            $product = Product::findOrFail($id);
+                if ($product->delete()) {
+                return response()->json(['product'=>$product],204);;
+                }
+        } catch (\Exception $e) {
+            return response()->json(['message'=>'Error Error: Not Found'],404);
         }
     }
 }
